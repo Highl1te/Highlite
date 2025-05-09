d343283f9776e3efe4f349828fdedfa7e637415e
KKonaOG <KKonaOG@fanet.dev>
2025-05-08 23:24:02 -0500

    Added better bundling dynamics and added a development loader to support Highlite core development

--- bundle.ts
+++ bundle.ts
@@ -1,12 +1,8 @@
-await Bun.build({
-    entrypoints: ['./src/index.ts'],
-    outdir: './dist',
-    naming: 'highlite.user.js',
-    minify: true,
-    banner: "// ==UserScript==\n\
-// @name         Highlite\n\
+const pJSON = require('./package.json')
+const UserBannerProd = `// ==UserScript==\n\
+// @name         Highlite Loader\n\
 // @namespace    Highlite\n\
-// @version      0.1.3\n\
+// @version      ${pJSON.version}\n\
 // @description  Rune-lite esque client for High Spell.\n\
 // @author       KKonaOG\n\
 // @match        https://highspell.com/game\n\
@@ -17,5 +13,48 @@ await Bun.build({
 // @grant        GM_getResourceText\n\
 // @grant        GM_xmlhttpRequest\n\
 // @run-at       document-start\n\
-// ==/UserScript=="
+// ==/UserScript==`
+
+// Production Build for Highlite Loader
+await Bun.build({
+    entrypoints: ['./src/index.ts'],
+    outdir: './dist',
+    naming: 'highlite.user.js',
+    minify: false,
+    banner: UserBannerProd,
+    env: "inline",
+    define: {
+        "process.env.NODE_ENV": "'production'"
+    }
+})
+
+
+const UserBannerDev = `// ==UserScript==\n\
+// @name         Highlite Loader Development\n\
+// @namespace    Highlite\n\
+// @version      ${pJSON.version}\n\
+// @description  Rune-lite esque client for High Spell.\n\
+// @author       KKonaOG\n\
+// @match        https://highspell.com/game\n\
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=highspell.com\n\
+// @connect      githubusercontent.com\n\
+// @connect      highspell.com\n\
+// @grant        GM_addElement\n\
+// @grant        GM_getResourceText\n\
+// @grant        GM_xmlhttpRequest\n\
+// @run-at       document-start\n\
+// ==/UserScript==`
+
+
+// Development Build for Highlite Loader
+await Bun.build({
+    entrypoints: ['./src/index.ts'],
+    outdir: './dist',
+    naming: 'highliteDevelopment.user.js',
+    minify: false,
+    banner: UserBannerDev,
+    env: "inline",
+    define: {
+        "process.env.NODE_ENV": "'development'"
+    }
 })
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ function preventClientExecution() {
     new MutationObserver((_, observer) => {
         const clientjsScriptTag = document.querySelector('script[src*="/js/client/client"]');
         if (clientjsScriptTag) {
+            console.log("[Highlite Loader] High Spell Client found, removing...");
             clientjsScriptTag.remove();
         }
     }).observe(document.documentElement, {
@@ -16,21 +17,24 @@ function preventClientExecution() {
 
 async function obtainHighliteCore() {
     let highliteCore = ""
-    const highliteCoreURL = "https://raw.githubusercontent.com/Highl1te/Core/refs/heads/main/dist/index.js";
+    const highliteCoreURL = "https://github.com/Highl1te/Core/releases/latest/download/index.js";
+    console.log("[Highlite Loader] Obtaining latest Highlite Core version.");
     highliteCore = (await GM.xmlHttpRequest({"method": "GET", "url": highliteCoreURL + "?time=" + Date.now()})).responseText;
+
     eval(highliteCore)
-    console.log("Highlite Core Loaded");
+    console.log("[Highlite Loader] Highlite Core Obtained.");
 };
 
 async function obtainHighSpellClient() {
     let highSpellClient = "";
     let highSpellAssetJSON = {};
     const assetJSONURL = "https://highspell.com:3002/assetsClient";
-
+    console.log("[Highlite Loader] Obtaining latest High Spell Client version.");
     highSpellAssetJSON = JSON.parse((await GM.xmlHttpRequest({"method": "GET", "url": assetJSONURL + "?time=" + Date.now()})).responseText);
     const highSpellClientURL = `https://highspell.com/js/client/client.${highSpellAssetJSON.data.latestClientVersion}.js`;
     highSpellClient = (await GM.xmlHttpRequest({"method": "GET", "url": highSpellClientURL + "?time=" + Date.now()})).responseText;
 
+    console.log("[Highlite Loader] High Spell Client Version: " + highSpellAssetJSON.data.latestClientVersion + " downloaded.");
     highSpellClient = highSpellClient.substring(0, highSpellClient.length - 9)
         + "; document.client = {};"
         + "document.client.get = function(a) {"
@@ -40,9 +44,8 @@ async function obtainHighSpellClient() {
         + "eval(a + ' = ' + b);"
         + "};"
         + highSpellClient.substring(highSpellClient.length - 9)
-
     eval(highSpellClient)
-    console.log("HighSpell Client Loaded");
+    console.log("[Highlite Loader] High Spell Client Injection Completed.");
 }
 
 // Entry Point
@@ -55,15 +58,24 @@ async function obtainHighSpellClient() {
         /*
             The reason why we are waiting for the DOM to be loaded is because the HighSpell client uses it
             as the startup event. To make sure we reload the script after the DOM has truly loaded, we specifically
-            fire it after the true DOMContentLoaded event. Tis allows Highlite to interact with the client before
-            it begins execution.
+            fire it after the true DOMContentLoaded event. This allows Highlite to interact with the client before
+            it begins execution. HighSpell is launcher within Highlite Core by firing another DOMContentLoaded event.
         */
        
         // doReact makes sure we don't react to our own DOMContentLoaded event
         if (doReact) {
             doReact = false; // Set to false to prevent infinite loop
             obtainHighSpellClient().then(() => { // This is the injected client script
-                obtainHighliteCore(); // This is the core script
+                // Check ENV mode to determine if we are in production or development mode
+                if (process.env.NODE_ENV === "production") {
+                    console.log("[Highlite Loader] Production mode detected, loading production core.");
+                    obtainHighliteCore(); // This is the core script
+                }
+                else if (process.env.NODE_ENV === "development") {
+                    console.log("[Highlite Loader] Development mode detected, core is expected to be loaded manually.");      
+                } else {
+                    console.error("[Highlite Loader] Unknown environment mode detected, aborting.");
+                }
             }) 
         }
     });
